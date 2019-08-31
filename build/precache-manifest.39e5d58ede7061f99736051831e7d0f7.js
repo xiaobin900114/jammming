7b61802497022b4b1f55dc15d0e2e1c747ad2c8b
xiaobin900114 <17800697+xiaobin900114@users.noreply.github.com>
2019-08-31 09:30:58 -0400

    add build

---
+++ build/precache-manifest.39e5d58ede7061f99736051831e7d0f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bbb8f3fc5b084e4514b23a0cea0af9d",
+    "url": "/jammming/index.html"
+  },
+  {
+    "revision": "ea2ed91127239857d73b",
+    "url": "/jammming/static/css/main.79756365.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/jammming/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "ea2ed91127239857d73b",
+    "url": "/jammming/static/js/main.2beea375.chunk.js"
+  },
+  {
+    "revision": "f5f9d50d5961f1446a57",
+    "url": "/jammming/static/js/runtime~main.85c7c486.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
